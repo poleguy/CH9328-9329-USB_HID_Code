bfea2f5f2fd71e2d6b6d1fac147fe861ae13d5c9
Nicholas Dietz <github@poleguy.com>
2021-02-12 14:14:12 -0600

    working on client/server

--- init.sh
+++ init.sh
@@ -3,28 +3,40 @@
 #  source ./init.sh
 #
 # running this directly without source will not effect the active environment
+#
 # https://stackoverflow.com/questions/496702/can-a-shell-script-set-environment-variables-of-the-calling-shell
+
+# this will create a clean ./cenv if it does not exist
+# it will run a quick update if it does, for interactive use.
+
 
 # .bashrc should be updated by running
 # conda init bash
 # conda config --set auto_activate_base false
 # conda config --set env_prompt '({name})'
 
-conda env create --prefix ./cenv -f environment.yml
-# if ./cenv already exists, the line above will error out with "CondaValueError: prefix already exists". The update line below will make sure it's up-to-date.
-conda env update --prefix ./cenv -f environment.yml
+# using || true to avoid erroring out if environment isn't happy
 
-# this will create a clean ./cenv if it does not exist
-# it will run a quick update if it does, for interactive use.
+conda env create --prefix ./cenv -f environment.yml || true
+# if ./cenv already exists, the line above will error out with "CondaValueError: prefix already exists". The update line below will make sure it's up-to-date.
+
+# update won't work if it's not already activated?
+conda env update --prefix ./cenv -f environment.yml || true
+# 
+# https://stackoverflow.com/questions/45510430/install-packages-into-existing-conda-environment-specified-in-environment-yml
+#Unfortunately if you have an environment name in environment.yml the update/install will happen to that named environment.
+
 
 # for debug
+echo "conda environments:"
 conda env list
 
 # activate
-conda activate ./cenv
+conda activate ./cenv || true
+
 
 # other helpful commands:
 # conda deactivate
 # conda env remove --prefix ./env
 # conda env remove --name test-env
-
+echo "init.sh done"
--- kvm_keys.py
+++ kvm_keys.py
@@ -4,18 +4,67 @@
 # run this program.
 # pick the port
 # typing F12 is the escape sequence to exit back to the menu
-# all keys will be sent to the selected 
+# all keys will be sent to the selected
+
+# specify --port /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0
+# specify remote target system with --remote hostname
+# in this mode it will capture the local keyboard and send the keystrokes
+# via ssh to the remote system 
+
+# when running as client
+# it opens an ssh connection to the remote
+# it copies the script there
+# sets up the environment and runs it as role = server
+#
+# the local client handles usbhid-dump
+# it creates usb strings and sends them to the server via the tunnel
+#
+# the server takes each message and sends it out the serial port
+
+import fileinput
 import serial
 import queue
 import time
 import subprocess
 import shlex
+import os
 
-def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"):
+def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0",
+         remote = None,
+         uid_vid = "03f0:034a",
+         timeout = 5000,
+         role = "client"):
     # use usbhid to grab keyboard
+
+    #if remote:
+        # generate a unique local port name
+        #if not os.path.exists('/root/dev'):
+        #    os.mkdir("/root/dev")
+        #vmodem = f"/root/dev/{os.path.basename(port)}"
+        #print(vmodem)
+        # https://unix.stackexchange.com/questions/504621/use-remote-serial-port-as-a-local-one
+        # dosen't work: serial.serialutil.SerialException: write failed: [Errno 5] Input/output error
+        #cmd = f'socat PTY,link={vmodem},rawer,wait-slave EXEC:"ssh {remote} socat - {port},nonblock,rawer"'
+        #cmd = shlex.split(cmd)
+        #socat_proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # it's remote, so pretend this is a local port
+        #port = vmodem
+        # wait till it should be ready
+        #for i in range(10):
+        #    if not os.path.exists(vmodem):
+        #        print("waiting")
+        #        time.sleep(1)
+        #print("ready")
+    # open the serial port
     ser = serial.Serial(port, baudrate=9600, timeout=0.5)
 
-    cmd = "usbhid-dump -m 03f0:034a -es -t 5000"
+    # this script needs to also be on the remote side
+    # this will copy the script to the remote side
+    # set up the environment
+    # run the server bit
+    
+    
+    cmd = f"usbhid-dump -m {uid_vid} -es -t {timeout}"
     cmd = shlex.split(cmd)
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     result = {}
@@ -47,9 +96,53 @@ def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"
     # wait till timeout
     proc.communicate()
 
+    if remote:
+        # close the remote serial port
+        socat_proc.kill()
+
     time.sleep(2) # just to be sure all is cleaned up and it doesn't destablize the os
 
+def server(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"):
+    # this runs on the machine with the usb serial port
+    # open the serial port
+    ser = serial.Serial(port, baudrate=9600, timeout=0.5)
 
+    # read usbhid-dump data and parse it
+    
+    result = {}
+    while True:
+    #for i in range(50):
+        
+        # https://stackoverflow.com/questions/1450393/how-do-you-read-from-stdin
+        row = fileinput.input()
+        if row == b'':
+            print("timeout detected")
+            break
+        print(row)
+        print('row done')
+        if b'STREAM' in row:
+            print('...')
+            values = proc.stdout.readline().decode("utf-8")
+            print(values)
+            values = values.split(' ')
+            codes = []
+            for v in values[1:]:
+                code = int(v, 16)
+                codes.append(code)
+            print(codes)
+            serial_data = codes_to_hid(codes)
+            if codes == [0,0,0x45,0,0,0,0,0]:
+                print('F12 pressed... ending session.')
+                break
+            print('.')
+            ser.write(serial_data)
+    print('done')
+    # wait till timeout
+    proc.communicate()
+
+    
+
+    
 
 def codes_to_hid(codes):
     serial_data = serial.to_bytes(codes)
@@ -57,5 +150,6 @@ def codes_to_hid(codes):
 
 
 if __name__ == '__main__':
-    main()
+    import typer
+    typer.run(server)
 
