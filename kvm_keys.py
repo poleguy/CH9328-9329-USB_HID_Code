686ec0bf47ec4436051d5feef83e69825608af58
Nicholas Dietz <github@poleguy.com>
2021-02-19 14:43:49 -0600

    work on permissions, clean exit, etc. modifier keys and function keys not working remotely

--- kvm_keys.py
+++ kvm_keys.py
@@ -6,6 +6,8 @@
 # typing F12 is the escape sequence to exit back to the menu
 # all keys will be sent to the selected
 
+#sudo python kvm_keys.py --role client --remote debussy.shurelab.com --username dietzn
+
 # specify --port /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0
 # specify remote target system with --remote hostname
 # in this mode it will capture the local keyboard and send the keystrokes
@@ -37,10 +39,11 @@ def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"
          remote = None,
          username = None,
          uid_vid = "03f0:034a",
-         timeout = 5000,
+         timeout = 30000, # msec
          role = "server"):
     # use usbhid to grab keyboard
-
+    print("prepraing connection...")
+    
     if "server" in role:
         server(port=port)
         return
@@ -48,7 +51,7 @@ def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"
     # running locally.
     # grab keyboard
 
-    if remote:
+    if "client" in role:
         # https://pexpect.readthedocs.io/en/stable/api/pxssh.html
         p = pxssh.pxssh()
         p.login(remote, username)
@@ -61,11 +64,34 @@ def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"
         print(p.before)
         p.sendline('python CH9328-9329-USB_HID_Code/kvm_keys.py')
         p.expect('kvm_keys> ')
-        
+
+
+    # set up permissions (write access to usb device)
+    # https://stackoverflow.com/questions/41238273/execute-shell-command-with-pipes-in-python
+    cmd = f"lsusb"
+    print(cmd)
+    cmd = shlex.split(cmd)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    response = proc.stdout.readlines()
+    for line in response:
+        line = line.decode('utf-8')
+        if uid_vid in line:
+            bus = line.split()[1]
+            device = line.split()[3][:-1]
+            break
+
+    cmd = f"sudo setfacl -m u:poleguy:rw /dev/bus/usb/{bus}/{device}"
+    #print(response)
+    print(cmd)
+    cmd = shlex.split(cmd)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.communicate()
+    
     cmd = f"usbhid-dump -m {uid_vid} -es -t {timeout}"
     print(cmd)
     cmd = shlex.split(cmd)
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
     # also open ssh connection
     #cmd = f'ssh dietzn@debussy.shurelab.com "source activate && conda activate ~/CH9328-9329-USB_HID_Code/cenv && echo blah && python3 CH9328-9329-USB_HID_Code/kvm_keys.py"'
@@ -74,6 +100,7 @@ def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"
 
 
     result = {}
+    print("ready for input:")
     while True:
     #for i in range(50):
         raw_row = proc.stdout.readline()
@@ -87,21 +114,42 @@ def main(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"
             print(f"far end: {p.before.decode('utf-8')}", flush=True)
             break
 
+        if b"00 00 45 00 00 00 00 00" in raw_row:
+            print("F12 pressed to exit 'b'",flush=True)
+            # eat the F12 so it doesn't get sent
+            # and fake out all buttons up to prevent stuck key
+            p.sendline('00 00 00 00 00 00 00 00')
+            p.expect("kvm_keys> ")
+            print(f"far end: {p.before.decode('utf-8')}", flush=True)
+            # then end it
+            p.sendline('No more')
+            p.prompt()
+            print(f"far end: {p.before.decode('utf-8')}", flush=True)
+            p.sendline('whoami')
+            p.prompt()
+            print(f"far end: {p.before.decode('utf-8')}", flush=True)
+            break
+
         print(f'near end: {row}', flush=True)
         p.sendline(row)
         p.expect("kvm_keys> ")
         print(f"far end: {p.before.decode('utf-8')}", flush=True)
-    print('done')
+
+    print('shutting down...')
 
     # look for response from far side
     #p.expect("No more interfaces to dump")
     if remote:
+        print("logging out")
         p.logout()
         
     # wait till timeout
-    proc.communicate()
+    #proc.communicate()
+    proc.terminate()
+    proc.wait()
 
     time.sleep(2) # just to be sure all is cleaned up and it doesn't destablize the os
+    print('done')
 
 def server(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0"):
     # this runs on the machine with the usb serial port
@@ -125,7 +173,8 @@ def server(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port
             print('kvm_keys> ', flush=True)
             row = f.readline()
             if 'No more' in row:
-                print("timeout detected", flush=True)
+                # timeout or stopped by client
+                print("end of keyboard input detected", flush=True)
                 break
             #print(row, flush=True)
             if 'STREAM' in row:
@@ -146,11 +195,13 @@ def server(port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port
                 #print('.', flush=True)
                 ser.write(serial_data)
     print('done', flush=True)
-    # wait till timeout
-    #proc.communicate()
+
 
     
-
+def test_codes_to_hid():
+    a = codes_to_hid([4,0,0,0,0,0,0,0])
+    print(a)
+    
     
 
 def codes_to_hid(codes):
